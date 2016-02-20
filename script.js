7eadb17f901b2a25f5ff713d59e2a3c78b10239c
decentninja <very@decent.ninja>
2016-02-20 21:34:07 +0100

    Better scrolling

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var SLICES_PER_SCREEN = 10;
-var FRICTION = 0.9;		// Speed kept per second
-var MAXIMUM_ADDED_SPEED_PER_SCROLL = 0.2;		// In screens per second
+var SPRING = 0.98;		// Speed kept per frame
+var SLIPSTICK = 1;	// Minimum speed
+var FINGER_FRICTION = 1;
+
 
 var ctx = document.querySelector(".maincanvas").getContext("2d");
 
@@ -8,22 +10,32 @@ var dpi = window.devicePixelRatio;
 if(dpi) ctx.scale(dpi, dpi);
 
 var appstate = {
-	position: 0,	// In screen
-	speed: 0		// In screens per second
+	position: 0,	// In slices
+	speed: 0		// In slices per second
 };
 
-function update_dimentions() {
-	ctx.canvas.width = window.innerWidth;
-	ctx.canvas.height = window.innerHeight;
+var dim = {
+	width: 0,
+	height: 0,
+	slice_height: 0
+};
+
+function update_dim() {
+	dim.width = ctx.canvas.width = window.innerWidth;
+	dim.height = ctx.canvas.height = window.innerHeight;
+	dim.slice_height = dim.height / SLICES_PER_SCREEN;
 }
 
-update_dimentions();
+update_dim();
 
-window.addEventListener("resize", update_dimentions);
+window.addEventListener("resize", update_dim);
 
 function update_positions(deltatime) {
 	appstate.position += appstate.speed * deltatime;
-	appstate.speed *= 1 - (1 - FRICTION) * deltatime;
+	appstate.speed *= SPRING;
+	if(Math.abs(appstate.speed) < SLIPSTICK) {
+		appstate.speed = 0;
+	}
 }
 
 function render_slices() {
@@ -31,17 +43,29 @@ function render_slices() {
 		var slice_number = Math.floor(appstate.position + i);
 		var slice_position = slice_number - appstate.position;
 		ctx.fillStyle = slice_number % 2 == 0 ? "green" : "blue";
-		ctx.fillRect(0, slice_position * ctx.canvas.height / SLICES_PER_SCREEN, ctx.canvas.width, ctx.canvas.height / SLICES_PER_SCREEN);
+		ctx.fillRect(0, slice_position * dim.slice_height, dim.width, dim.slice_height);
 	}
 }
 
+var start_touch, original_speed, touch_time;
+window.addEventListener("touchstart", function(e) {
+	start_touch = e.changedTouches[0].clientY;
+	touch_time = new Date();
+	original_speed = appstate.speed;
+});
 
-function scroll(e) {
-	appstate.speed += MAXIMUM_ADDED_SPEED_PER_SCROLL;
-}
+window.addEventListener("touchmove", function(e) {
+	//appstate.speed = original_speed + (start_touch - e.changedTouches[0].clientY) / dim.slice_height;
+});
 
-window.addEventListener("touchmove", scroll):
-window.addEventListener("wheel", scroll):
+window.addEventListener("touchend", function(e) {
+	var deltatime = ((Date.now()) - touch_time) / 1000;
+	appstate.speed = (start_touch - e.changedTouches[0].clientY) / (dim.slice_height * deltatime) * FINGER_FRICTION;
+});
+
+window.addEventListener("wheel", function(e) {
+	appstate.speed += e.deltaY / dim.slice_height * FINGER_FRICTION;
+});
 
 var lastframetime = Date.now();
 function update() {
