b53729ba805d688e11776a6500e52630606645a4
decentninja <very@decent.ninja>
2016-02-28 10:49:02 +0100

    Fullscreen

--- script.js
+++ script.js
@@ -110,3 +110,17 @@ function update() {
 }
 
 window.requestAnimationFrame(update);
+
+window.addEventListener("click", function(e) {
+	e.preventDefault();
+	var elem = document.documentElement;
+	if (elem.requestFullscreen) {
+		elem.requestFullscreen();
+	} else if (elem.msRequestFullscreen) {
+		elem.msRequestFullscreen();
+	} else if (elem.mozRequestFullScreen) {
+		elem.mozRequestFullScreen();
+	} else if (elem.webkitRequestFullscreen) {
+		elem.webkitRequestFullscreen();
+	}
+});
