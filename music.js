1360483940de92ade647aa90e0c54702483ab7c8
decentninja <very@decent.ninja>
2016-02-28 15:00:49 +0100

    Wat

--- music.js
+++ music.js
@@ -17,7 +17,7 @@ var current_track = "";
 
 var maintrack = {
     update: function(appstate) {
-        var mobile = true;///android|iphone|ipad/i.test(navigator.userAgent);
+        var mobile = /android|iphone|ipad/i.test(navigator.userAgent);
         if(!mobile) {
             if(appstate.menu) {
                 for(var key in tracks) {
@@ -51,8 +51,5 @@ var maintrack = {
                 }
             }
         }
-        
     }
 };
-
-
